615523e5dc77becb0d603b14bfb02d8a3bc503ad
Allan Paixão Tavares <99320857+Allanpt@users.noreply.github.com>
2024-04-08 13:45:43 -0300

    Update src/components/Person/Person.jsx
    Co-authored-by: Misha Dzhemula <98764641+DarkMistyRoom@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
 
-      {person.age !== undefined && (
+      {person.age && (
         <p className="Person__age">I am {person.age}</p>
       )}
 
